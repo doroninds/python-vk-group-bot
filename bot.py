b2ca336a0d8a2ec0e5862a7f44f69b99aa5b2e35
Dmitriy Doronin <dmitriy435@gmail.com>
2022-02-05 10:32:15 +0000

    :recycle: exceot long pull

--- bot.py
+++ bot.py
@@ -7,34 +7,43 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from library.vk import VkBotMessanger
 from task_manager import TaskManager
 
+
+class MyVkLongPoll(VkBotLongPoll):
+    def listen(self):
+        while True:
+            try:
+                for event in self.check():
+                    yield event
+            except Exception as e:
+                print('error', e)
+                bot_messanger.send_message(
+                    settings.DEFAULT_PEER_ID, settings.ALERT_MESSAGE)
+
+
 # инициализация vk api
 vk = vk_api.VkApi(token=settings.VK_API_TOKEN)
 session = vk.get_api()
 
 # инициализация longpoll бота
-botLongpoll = VkBotLongPoll(vk=vk, group_id=settings.VK_BOT_GROUP_ID)
+botLongpoll = MyVkLongPoll(vk=vk, group_id=settings.VK_BOT_GROUP_ID)
 
 bot_messanger = VkBotMessanger(vk)
 task_manager = TaskManager(bot_messanger)
 
+
 # Слушаем longpoll(Сообщения)
-try:
-    for event in botLongpoll.listen():
-        try:
-    
+for event in botLongpoll.listen():
+    try:
             if(event.message.text == ''):
                 continue
 
             if(event.message.text[0] != '!'):
                 continue
-            
+
             print('event', event)
 
             if event.type == VkBotEventType.MESSAGE_NEW:
                 commander = Commander(event)
                 task_manager.process_command(commander)
-
-        except Exception:
+    except Exception:
             traceback.print_exc()
-except:
-    bot_messanger.send_message(settings.DEFAULT_PEER_ID, settings.ALERT_MESSAGE)
